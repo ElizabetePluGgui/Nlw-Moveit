caf732023e00d8e73456070eff1e8139e0e0ef7c
Elizabete Fabri <elizabete958@gmail.com>
2021-02-24 19:40:57 -0300

    OK

--- moveit/src/App.tsx
+++ moveit/src/App.tsx
@@ -1,11 +1,9 @@
-import { Button } from './components/Button';
+import './styles/global.css';
 
 function App() {
   return (
     <div>
-      <Button />
-      <Button />
-      <Button />
+      <button style={{ height: '2rem' }}>TESTE</button>
     </div>
   );
 }
