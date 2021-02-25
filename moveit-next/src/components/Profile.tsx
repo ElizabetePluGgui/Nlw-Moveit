c7369bc8786fc74f8575c425a5d6df4440ad3c00
Elizabete Fabri <elizabete958@gmail.com>
2021-02-24 22:34:21 -0300

    ok

---
+++ moveit-next/src/components/Profile.tsx
@@ -0,0 +1,16 @@
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img
+        src="https://avatars.githubusercontent.com/u/67619417?s=460&u=bc9151a9a009071ebef8c49d9140247fda5340db&v=4"
+        alt="Elizabete Fabri"
+      />
+      <div>
+        <strong>Elizabete de Sousa Fabri</strong>
+        <p>Level 1</p>
+      </div>
+    </div>
+  );
+}
